98ec3ab941bc3a7b4cb90362d223794f8d9e6346
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-02-13 00:24:43 +0900

    feat: styling with css

--- src/entrypoints/injection.ts
+++ src/entrypoints/injection.ts
@@ -5,6 +5,39 @@ export default defineUnlistedScript(main);
 function main() {
   const observer = new MutationObserver(ensurePlayAllButton);
 
+  const style = document.createElement("style");
+  style.textContent = `
+    .play-all-btn {
+      background-color: #8000FF;
+      color: #F1F1F1;
+
+      height: 32px;
+      min-width: 12px;
+
+      display: inline-flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      margin-left: 12px;
+
+      border-radius: 8px;
+      padding: 0 12px;
+
+      font-family: 'Roboto', 'Arial', sans-serif;
+      font-size: 1.4rem;
+      font-weight: 500;
+      text-decoration: none;
+
+      cursor: pointer;
+    }
+
+    .play-all-btn:hover,
+    .play-all-btn:focus {
+      background-color:#9B33FF;
+    }
+  `;
+  document.head.appendChild(style);
+
   // Triggered when navigating to the videos, shorts, or streams page
   window.addEventListener("yt-navigate-finish", () => {
     const videoKind = window.location.pathname.split("/").at(-1) ?? "";
