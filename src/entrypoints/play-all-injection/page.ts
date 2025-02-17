1fb843f73a314264f69ceba42948ebe0625e555d
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-02-17 22:25:15 +0900

    refactor: use Page class

--- src/entrypoints/play-all-injection/page.ts
+++ src/entrypoints/play-all-injection/page.ts
@@ -1,16 +1,5 @@
-﻿export default class Playlist {
-  public channelId: string;
-  public videoKind: VideoKind;
-  public sortKind: SortKind;
-  public path: string;
-  public constructor() {
-    this.channelId = this._getChannelId();
-    this.videoKind = this._getVideoKind();
-    this.sortKind = this._getSortKind();
-    this.path = this._getPlayListPath();
-  }
-
-  private _getChannelId(): string {
+export default class Page {
+  private static get channelId(): string {
     return document
       .querySelector<HTMLLinkElement>("link[rel='canonical']")!
       .href.split("/")
@@ -18,7 +7,7 @@
       .slice(2);
   }
 
-  private _getVideoKind(): VideoKind {
+  public static get videoKind(): VideoKind {
     const videoKind = window.location.pathname.split("/").at(-1);
     switch (videoKind) {
       case "videos":
@@ -28,11 +17,11 @@
       case "streams":
         return "Streams";
       default:
-        return "Videos";
+        return null;
     }
   }
 
-  private _getSortKind(): SortKind {
+  public static get sortKind(): SortKind {
     const selectedButton = document.querySelector("#primary #header #chips>[selected]")!;
     const index = selectedButton
       ? Array.from(selectedButton.parentNode?.children ?? []).indexOf(selectedButton)
@@ -49,7 +38,62 @@
     }
   }
 
-  private _getPlayListPath(): string {
+  public static get hasPlayAllButton() {
+    return !!document.querySelector(".play-all-btn");
+  }
+
+  public static applyStyleForPlayAllButton() {
+    const style = document.createElement("style");
+    style.textContent = `
+    .play-all-btn {
+      background-color: #8000FF;
+      color: #F1F1F1;
+
+      height: 32px;
+      min-width: 12px;
+
+      display: inline-flex;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      margin-left: 12px;
+
+      border-radius: 8px;
+      padding: 0 12px;
+
+      font-family: 'Roboto', 'Arial', sans-serif;
+      font-size: 1.4rem;
+      font-weight: 500;
+      text-decoration: none;
+
+      cursor: pointer;
+    }
+
+    .play-all-btn:hover,
+    .play-all-btn:focus {
+      background-color:#9B33FF;
+    }
+  `;
+    document.head.appendChild(style);
+  }
+
+  public static ensurePlayAllButton() {
+    if (!this.hasPlayAllButton) {
+      this.addPlayAllButton();
+    }
+  }
+
+  public static addPlayAllButton() {
+    const playAllButton = document.createElement("a");
+    playAllButton.classList.add("play-all-btn");
+    playAllButton.href = this.getPlayListPath();
+    playAllButton.textContent = `Play All (${this.sortKind})`;
+
+    const buttonHolder = document.querySelector("#primary #header #chips")!;
+    buttonHolder.appendChild(playAllButton);
+  }
+
+  public static getPlayListPath(): string {
     if (this.sortKind === "Oldest") {
       const oldestVideoHref = document.querySelector<HTMLLinkElement>(
         "#thumbnail[href^='/watch?v=']",
@@ -61,7 +105,7 @@
     }
   }
 
-  private _getPlayListPrefix(videoKind: VideoKind, sortKind: SortKind): string {
+  private static _getPlayListPrefix(videoKind: VideoKind, sortKind: SortKind): string {
     switch (true) {
       case videoKind === "Videos" && sortKind === "Latest":
         return "UULF";
@@ -81,5 +125,5 @@
   }
 }
 
-type VideoKind = "Videos" | "Shorts" | "Streams";
+type VideoKind = "Videos" | "Shorts" | "Streams" | null;
 type SortKind = "Latest" | "Popular" | "Oldest";
