ebc477d5cf97ebdb36ea98f69bf6bfa61e869ad7
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-02-18 16:28:01 +0900

    style: format indent

--- src/entrypoints/content/page.ts
+++ src/entrypoints/content/page.ts
@@ -41,35 +41,35 @@ export default class Page {
   public static applyStyleForPlayAllButton() {
     const style = document.createElement("style");
     style.textContent = `
-    .play-all-btn {
-      background-color: #8000FF;
-      color: #F1F1F1;
+      .play-all-btn {
+        background-color: #8000FF;
+        color: #F1F1F1;
 
-      height: 32px;
-      min-width: 12px;
+        height: 32px;
+        min-width: 12px;
 
-      display: inline-flex;
-      flex-direction: row;
-      align-items: center;
-      justify-content: center;
-      margin-left: 12px;
+        display: inline-flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        margin-left: 12px;
 
-      border-radius: 8px;
-      padding: 0 12px;
+        border-radius: 8px;
+        padding: 0 12px;
 
-      font-family: 'Roboto', 'Arial', sans-serif;
-      font-size: 1.4rem;
-      font-weight: 500;
-      text-decoration: none;
+        font-family: 'Roboto', 'Arial', sans-serif;
+        font-size: 1.4rem;
+        font-weight: 500;
+        text-decoration: none;
 
-      cursor: pointer;
-    }
+        cursor: pointer;
+      }
 
-    .play-all-btn:hover,
-    .play-all-btn:focus {
-      background-color:#9B33FF;
-    }
-  `;
+      .play-all-btn:hover,
+      .play-all-btn:focus {
+        background-color:#9B33FF;
+      }
+    `;
     document.head.appendChild(style);
   }
 
