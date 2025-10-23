df7e26412f2d6aec69a67b93cf1eef464d992fdf
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-23 21:56:06 +0900

    test: add channel id spec

---
+++ tests/channel-id.spec.ts
@@ -0,0 +1,85 @@
+import { ytTest } from "./utils";
+
+type ChannelIdTestCase = {
+  channelButtonSelector: string;
+  existsOnHtml: boolean;
+  existsOnEvent: boolean;
+};
+
+const searchWords = ["music", "game"];
+
+searchWords.forEach((searchWord, i) => {
+  const channelIdTestCases: ChannelIdTestCase[] = [
+    {
+      channelButtonSelector: "#channel-info #channel-name",
+      existsOnHtml: false,
+      existsOnEvent: true,
+    },
+    {
+      channelButtonSelector: "#channel-thumbnail", // [href^="/channel/"] or [href^="link:///"]
+      existsOnHtml: true,
+      existsOnEvent: true,
+    },
+  ];
+  channelIdTestCases.forEach((channelIdTestCase, j) => {
+    const { channelButtonSelector, existsOnHtml, existsOnEvent } =
+      channelIdTestCase;
+    ytTest(
+      `channelId: soft navigation ${i}-${j}`,
+      async ({ page, eventWatcher, channelIdFinder }) => {
+        await page.goto(
+          `https://www.youtube.com/results?search_query=${searchWord}`,
+        );
+        await eventWatcher.waitForFired("yt-navigate-finish");
+
+        await page.locator(channelButtonSelector).first().click();
+        await eventWatcher.waitForFired("yt-navigate-finish");
+
+        await channelIdFinder.exceptFromNavigationEvent(existsOnEvent);
+        await channelIdFinder.exceptFromCanonicalLink(existsOnHtml);
+        await channelIdFinder.exceptFromYtInitialData(existsOnHtml);
+        await channelIdFinder.exceptFromYtCommand(existsOnHtml);
+      },
+    );
+  });
+});
+
+searchWords.forEach((searchWord, i) => {
+  const channelIdTestCases: ChannelIdTestCase[] = [
+    {
+      channelButtonSelector: "#channel-info #channel-name [href]",
+      existsOnHtml: true,
+      existsOnEvent: true,
+    },
+    {
+      channelButtonSelector: "#channel-thumbnail", // [href^="/channel/"] or [href^="link:///"]
+      existsOnHtml: true,
+      existsOnEvent: true,
+    },
+  ];
+  channelIdTestCases.forEach((channelIdTestCase, j) => {
+    const { channelButtonSelector, existsOnHtml, existsOnEvent } =
+      channelIdTestCase;
+    ytTest(
+      `channelId: hard navigation ${i}-${j}`,
+      async ({ page, eventWatcher, channelIdFinder }) => {
+        await page.goto(
+          `https://www.youtube.com/results?search_query=${searchWord}`,
+        );
+        await eventWatcher.waitForFired("yt-navigate-finish");
+
+        const url = await page
+          .locator(channelButtonSelector)
+          .first()
+          .getAttribute("href");
+        await page.goto(`https://www.youtube.com${url}`);
+        await eventWatcher.waitForFired("yt-navigate-finish");
+
+        await channelIdFinder.exceptFromNavigationEvent(existsOnEvent);
+        await channelIdFinder.exceptFromCanonicalLink(existsOnHtml);
+        await channelIdFinder.exceptFromYtInitialData(existsOnHtml);
+        await channelIdFinder.exceptFromYtCommand(existsOnHtml);
+      },
+    );
+  });
+});
