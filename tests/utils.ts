0ca15de83c761049ebe1c59b740c8bdd42d5ca45
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-23 23:49:12 +0900

    refactor: remove unused export

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,7 +1,7 @@
 import type { Page } from "@playwright/test";
 import { expect, test } from "@playwright/test";
 
-export class EventWatcher {
+class EventWatcher {
   private page: Page;
 
   constructor(page: Page) {
