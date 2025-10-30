f8e007ba3486b30f40db1b00e70a2ee5fa131744
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2025-10-30 14:55:03 +0900

    refactor: improve readability

--- src/entrypoints/content/index.ts
+++ src/entrypoints/content/index.ts
@@ -1,58 +1,22 @@
 ﻿import { defineContentScript } from "#imports";
-import ChannelPage from "./channel-page";
+import { CategoryPage } from "./category-page";
 import YoutubePage from "./youtube-page";
 
 export default defineContentScript({
   matches: ["https://www.youtube.com/*"],
   runAt: "document_end",
-  main,
+  main: onYoutubeActivated,
 });
 
-async function main() {
-  YoutubePage.applyStyleForPlayAllButton();
-
-  if (ChannelPage.isOnSupportedPage) {
-    const res = await fetch(window.location.href);
-    const html = await res.text();
-    const match = html.match(/<link rel="canonical" href="(.*?)"/i);
-    const canonical = match![1];
-    const channelId = canonical.split("/").at(-1)!;
-
-    const channelPage = new ChannelPage(channelId);
-    channelPage.ensurePlayAllButton();
+async function onYoutubeActivated() {
+  YoutubePage.addStyleForPlayAllButton();
+  if (CategoryPage.isCategoryPage) {
+    await CategoryPage.mount();
   }
 
-  // Triggered when navigating to the videos, shorts, or streams page
-  window.addEventListener("yt-navigate-finish", (e: any) => {
-    if (!ChannelPage.isOnSupportedPage) {
-      return;
+  window.addEventListener("yt-navigate-finish", async () => {
+    if (CategoryPage.isCategoryPage) {
+      await CategoryPage.mount();
     }
-
-    const channelId = e.detail.endpoint.browseEndpoint.browseId;
-    const channelPage = new ChannelPage(channelId);
-    channelPage.ensurePlayAllButton();
-
-    const observer = new MutationObserver(() => {
-      if (ChannelPage.isOnSupportedPage) {
-        channelPage.ensurePlayAllButton();
-      }
-    });
-
-    // Callback will be triggered when changing the sort to latest/popular/oldest
-    const buttonHolder = document.querySelector("#primary #header #chips")!;
-    observer.observe(buttonHolder, {
-      subtree: true,
-      childList: false,
-      attributes: true,
-    });
-
-    // Triggered when navigating to the videos, shorts, or streams page
-    window.addEventListener(
-      "yt-navigate-start",
-      () => {
-        observer.disconnect();
-      },
-      { once: true },
-    );
   });
 }
