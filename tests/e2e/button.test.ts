c5033865b7f5fe455824114ba5203baf0483024e
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-02-23 13:58:25 +0900

    test: split e2e.test.ts to button.test.ts and playlist.test.ts

--- tests/e2e/button.test.ts
+++ tests/e2e/button.test.ts
@@ -1,8 +1,7 @@
-import { expect } from "@playwright/test";
 import { CategoryTab } from "@/entrypoints/play-all-button.content/category-tab";
 import { SortTab } from "@/entrypoints/play-all-button.content/sort-tab";
 import { ytxTest } from "../fixture";
-import { YtChannelPage, YtPage, YtSearchPage, YtVideoPage } from "../utils";
+import { YtChannelPage, YtSearchPage } from "../utils";
 
 const channel = "@Microsoft";
 const searchNavigationModes: ("soft" | "hard")[] = ["soft", "hard"];
@@ -55,36 +54,5 @@ searchNavigationModes.forEach((searchNavigationMode) => {
         },
       );
     });
-  });
-});
-
-CategoryTab.categories.forEach((category) => {
-  SortTab.sorts.forEach((sort) => {
-    ytxTest(
-      `Playlist: ${category} - ${sort}`,
-      async ({ page, eventWatcher }) => {
-        ytxTest.skip(
-          category === "Shorts" && sort === "Popular",
-          "Populared Shorts page has not been updated by YouTube",
-        );
-        const ytChannelPage = new YtChannelPage(channel, page, eventWatcher);
-        await ytChannelPage.navigateToCategory(category, "hard");
-        await ytChannelPage.navigateToSort(sort);
-        const playlistUrl = await ytChannelPage.getPlayAllUrl(category, sort);
-        const topVideoIds = await ytChannelPage.getTopVideoIds(3);
-
-        await page.goto(playlistUrl);
-        const ytPage = new YtPage(page, eventWatcher);
-        await eventWatcher.waitForFired(ytPage.NavigationEndEvent);
-
-        const ytVideoPage = new YtVideoPage(page);
-        const n = sort !== "Oldest" ? 3 : 1;
-        const playlistVideoIds =
-          sort !== "Oldest"
-            ? await ytVideoPage.getPlaylistVideoIds(n)
-            : [await ytVideoPage.getPlaylistSelectedVideoId()];
-        expect(playlistVideoIds).toEqual(topVideoIds.slice(0, n));
-      },
-    );
   });
 });
