ce92fceec326dd47568dccb83716e2b1dc8e59cb
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-04-01 16:12:08 +0900

    refactor!: fetch oldest video id from playlist instead of innertube

--- src/entrypoints/play-all-button.content/youtube-api.ts
+++ src/entrypoints/play-all-button.content/youtube-api.ts
@@ -57,15 +57,28 @@ async function getOldestItemId(
   channelUrl: string,
   categoryKind: CategoryKind,
 ): Promise<string> {
-  const htmlRes = await fetch(channelUrl);
+  const playlistPrefix = resolvePlaylistPrefix(categoryKind, "Latest");
+  const channelId = (await fetchChannelId(channelUrl))!;
+  const playlistUrl = `${window.location.origin}/playlist?list=${playlistPrefix}${channelId.slice(2)}&hl=en&persist_hl=1`;
+  const videoCount = (await fetchYtInitialData(playlistUrl)).header
+    .playlistHeaderRenderer.stats[0].runs[0].text;
+  logger.info({ videoCount });
+
+  const oldestVideoUrl = `${window.location.origin}/playlist?list=${playlistPrefix}${channelId!.slice(2)}&index=${videoCount}&playnext=1`;
+  const oldestVideoId = (await fetchYtInitialData(oldestVideoUrl))
+    .currentVideoEndpoint.watchEndpoint.videoId;
+  logger.info({ oldestVideoId });
+  return oldestVideoId;
+}
+
+async function fetchYtInitialData(url: string) {
+  const htmlRes = await fetch(url);
   const html = await htmlRes.text();
-  const clientVersion = html.match(/"clientVersion":"(.*?)"/)![1];
   const ytInitialDataString = html.match(
     YoutubePage.isMobile
       ? /var ytInitialData\s*=\s*'([\s\S]*?)';/
       : /var ytInitialData\s*=\s*(\{[\s\S]*?\});/,
   )![1];
-
   const ytInitialData = JSON.parse(
     YoutubePage.isMobile
       ? ytInitialDataString
@@ -74,106 +87,6 @@ async function getOldestItemId(
             String.fromCharCode(parseInt(hex, 16)),
           )
       : ytInitialDataString,
-  ) as YtInitialData;
-  const continuationToken = extractContinuationToken(
-    ytInitialData,
-    categoryKind,
-    "Oldest",
   );
-
-  const jsonRes = await fetch("https://www.youtube.com/youtubei/v1/browse", {
-    method: "POST",
-    body: JSON.stringify({
-      context: {
-        client: {
-          clientName: "WEB",
-          clientVersion: clientVersion,
-        },
-      },
-      continuation: continuationToken,
-    }),
-  });
-  const browseResponse = (await jsonRes.json()) as BrowseResponse;
-
-  const itemsData = browseResponse.onResponseReceivedActions.find(
-    (action) =>
-      action.reloadContinuationItemsCommand.slot ===
-      "RELOAD_CONTINUATION_SLOT_BODY",
-  )!;
-  const topVideoId = JSON.stringify(itemsData)
-    .match(/"videoId":"(.*?)"/i)
-    ?.at(1);
-
-  logger.info("getOldestItemId()", { topVideoId });
-  return topVideoId!;
+  return ytInitialData;
 }
-
-function extractContinuationToken(
-  ytInitialData: YtInitialData,
-  category: CategoryKind,
-  sortKind: SortKind,
-) {
-  const tabs = (ytInitialData.contents.twoColumnBrowseResultsRenderer ??
-    ytInitialData.contents.singleColumnBrowseResultsRenderer)!.tabs;
-  const tabRenderers = tabs.map((tab) => tab.tabRenderer);
-  const categoryTabRenderer = tabRenderers.find((tab) =>
-    tab.endpoint.commandMetadata.webCommandMetadata.url.endsWith(
-      category.toLowerCase(),
-    ),
-  )!;
-  const rendererHeader = categoryTabRenderer.content.richGridRenderer.header;
-  const tokens = Array.from(
-    JSON.stringify(rendererHeader).matchAll(/"token":"(.*?)"/g),
-  ).map((match) => match[1]);
-  const [latestVideoToken, popularVideoToken, oldestVideoToken] = tokens;
-  const token = (() => {
-    switch (sortKind) {
-      case "Latest":
-        return latestVideoToken;
-      case "Popular":
-        return popularVideoToken;
-      case "Oldest":
-        return oldestVideoToken;
-    }
-  })();
-  logger.info("extractContinuationToken()", { token });
-  return token;
-}
-
-type YtInitialData = {
-  contents: {
-    //desktop
-    twoColumnBrowseResultsRenderer?: {
-      tabs: Tab[];
-    };
-    // mobile
-    singleColumnBrowseResultsRenderer?: {
-      tabs: Tab[];
-    };
-  };
-};
-
-type Tab = {
-  tabRenderer: {
-    endpoint: {
-      commandMetadata: {
-        webCommandMetadata: {
-          url: string;
-        };
-      };
-    };
-    content: {
-      richGridRenderer: {
-        header: object;
-      };
-    };
-  };
-};
-
-type BrowseResponse = {
-  onResponseReceivedActions: Array<{
-    reloadContinuationItemsCommand: {
-      slot: "RELOAD_CONTINUATION_SLOT_HEADER" | "RELOAD_CONTINUATION_SLOT_BODY";
-    };
-  }>;
-};
