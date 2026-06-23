053e160aaa14c390ddf7073cee66e3e1601058b5
CatBraaain <84499939+CatBraaain@users.noreply.github.com>
2026-06-23 13:10:44 +0900

    ci: use vp instead of biome and sort-package-json

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  fmt: {
+    sortImports: true,
+    sortPackageJson: {
+      sortScripts: true,
+    },
+  },
+  lint: {
+    rules: {
+      "unicorn/prefer-node-protocol": "warn",
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          fix: {
+            imports: "off",
+            variables: "off",
+          },
+        },
+      ],
+    },
+  },
+});
